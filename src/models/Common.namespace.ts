bc69b9228be91f4e288ed70247c2e369336b971a
Aleksandr.Sustavov <bazaster1@gmail.com>
2020-07-14 17:35:28 +0300

    add more materials

--- src/models/Common.namespace.ts
+++ src/models/Common.namespace.ts
@@ -1,10 +1,14 @@
 export namespace CommonNamespace {
 
   export enum MaterialsEnum {
+    fabric = 'fabric',
     wood = 'wood',
+    glass = 'glass',
     iron = 'iron',
     steel = 'steel',
     silver = 'silver',
     gold = 'gold',
+    mithril = 'mithril',
+    ebonite = 'ebonite',
   }
 }
