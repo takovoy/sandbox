97fdd06f6d63b409949c9f44beeaa5fe73820956
takovoy <bazaster1@gmail.com>
2020-07-20 08:51:46 +0300

    first texture

--- src/stubs/game-field.stub.ts
+++ src/stubs/game-field.stub.ts
@@ -1,10 +1,18 @@
 import {GameFieldModelNamespace} from '../models/GameFieldModel.namespace';
+import GameFieldCellTypesEnum = GameFieldModelNamespace.GameFieldCellTypesEnum;
 
 export const GAME_FIELD: GameFieldModelNamespace.IGameField = {
   id: 'test game level',
-  name: 'Than is test level',
-  description: 'Than is test level',
-  cells: [],
+  name: 'That is the test level',
+  description: 'That is the test level',
+  cells: [
+    {
+      animationFramesCount: 1,
+      positionX: 1,
+      positionY: 1,
+      type: GameFieldCellTypesEnum.grass,
+    }
+  ],
   inventoryItems: [],
   landscapeItems: [],
   sizeX: 10,
